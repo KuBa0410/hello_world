85a7433fff79b395ac02595ea334e94d2caedb45
domin <domin@LAPTOP-HVE76DVR>
2017-04-28 21:14:55 +0200

    insert a comment

---
+++ second_test/main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: domin
+ *
+ * Created on 28. April 2017, 21:14
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "hello world \n";
+    return 0;
+}
+
